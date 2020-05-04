38d90f0d928df31a7954c70cbff5605deeb79441
root <root@dev1VM.ubuntu.com>
2020-05-04 06:50:15 -0400

    1st commit

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
+// Just adding comment
 require 'config.php';
 require 'functions.php';
 
+// Just adding comment
+// Just adding comment
+// Just adding comment
+// Just adding comment
+// Just adding comment
+// Just adding comment
 run();
